c30c862f8cf3239c4246252c396566579b20097c
lare <74225315+laresamdeola@users.noreply.github.com>
2022-01-27 08:08:47 +0000

    Add the links from tribes starting with A all the way to Z. Finished the javascript code for the home.html as regards changing the text dynamically to aid user interaction and information.

--- home.js
+++ home.js
@@ -17,6 +17,213 @@ for(i = 0; i <= 25; i++){
       b.addEventListener("mouseout", () => {
         b.innerText = "BEROM";
       });
+
+    case 2:
+      let c = document.getElementById("starting-with-c");
+      c.addEventListener("mouseover", () => {
+        c.innerText = "C";
+      });
+      c.addEventListener("mouseout", () => {
+        c.innerText = "CHIBOK";
+      });
+
+    case 3:
+      let d = document.getElementById("starting-with-d");
+      d.addEventListener("mouseover", () => {
+        d.innerText = "D";
+      });
+      d.addEventListener("mouseout", () => {
+        d.innerText = "DARKARKARI";
+      });
+
+    case 4:
+      let e = document.getElementById("starting-with-e");
+      e.addEventListener("mouseover", () => {
+        e.innerText = "E";
+      });
+      e.addEventListener("mouseout", () => {
+        e.innerText = "EBIRA";
+      });
+
+    case 5:
+      let f = document.getElementById("starting-with-f");
+      f.addEventListener("mouseover", () => {
+        f.innerText = "F";
+      });
+      f.addEventListener("mouseout", () => {
+        f.innerText = "FULANI";
+      });
+
+    case 6:
+      let g = document.getElementById("starting-with-g");
+      g.addEventListener("mouseover", () => {
+        g.innerText = "G";
+      });
+      g.addEventListener("mouseout", () => {
+        g.innerText = "GWARI";
+      });
+
+    case 7:
+      let h = document.getElementById("starting-with-h");
+      h.addEventListener("mouseover", () => {
+        h.innerText = "H";
+      });
+      h.addEventListener("mouseout", () => {
+        h.innerText = "HAUSA";
+      });
+
+    case 8:
+      let i = document.getElementById("starting-with-i");
+      i.addEventListener("mouseover", () => {
+        i.innerText = "I";
+      });
+      i.addEventListener("mouseout", () => {
+        i.innerText = "IGBO";
+      });
+
+    case 9:
+      let j = document.getElementById("starting-with-j");
+      j.addEventListener("mouseover", () => {
+        j.innerText = "j";
+      });
+      j.addEventListener("mouseout", () => {
+        j.innerText = "JUKUN";
+      });
+
+    case 10:
+      let k = document.getElementById("starting-with-k");
+      k.addEventListener("mouseover", () => {
+        k.innerText = "K";
+      });
+      k.addEventListener("mouseout", () => {
+        k.innerText = "KALABARI";
+      });
+
+    case 11:
+      let l = document.getElementById("starting-with-l");
+      l.addEventListener("mouseover", () => {
+        l.innerText = "l";
+      });
+      l.addEventListener("mouseout", () => {
+        l.innerText = "LUA";
+      });
+
+    case 12:
+      let m = document.getElementById("starting-with-m");
+      m.addEventListener("mouseover", () => {
+        m.innerText = "M";
+      });
+      m.addEventListener("mouseout", () => {
+        m.innerText = "MBEMBE";
+      });
+
+    case 13:
+      let n = document.getElementById("starting-with-n");
+      n.addEventListener("mouseover", () => {
+        n.innerText = "N";
+      });
+      n.addEventListener("mouseout", () => {
+        n.innerText = "NUPE";
+      });
+
+    case 14:
+      let o = document.getElementById("starting-with-o");
+      o.addEventListener("mouseover", () => {
+        o.innerText = "O";
+      });
+      o.addEventListener("mouseout", () => {
+        o.innerText = "OGONI";
+      });
+
+    case 15:
+      let p = document.getElementById("starting-with-p");
+      p.addEventListener("mouseover", () => {
+        p.innerText = "P";
+      });
+      p.addEventListener("mouseout", () => {
+        p.innerText = "PAI";
+      });
+
+    case 16:
+      let q = document.getElementById("starting-with-q");
+      q.addEventListener("mouseover", () => {
+        q.innerText = "Q";
+      });
+      q.addEventListener("mouseout", () => {
+        q.innerText = "QUA";
+      });
+
+    case 17:
+      let r = document.getElementById("starting-with-r");
+      r.addEventListener("mouseover", () => {
+        r.innerText = "R";
+      });
+      r.addEventListener("mouseout", () => {
+        r.innerText = "RUBU";
+      });
+
+    case 18:
+      let s = document.getElementById("starting-with-s");
+      s.addEventListener("mouseover", () => {
+        s.innerText = "s";
+      });
+      s.addEventListener("mouseout", () => {
+        s.innerText = "SUKUR";
+      });
+
+    case 19:
+      let t = document.getElementById("starting-with-t");
+      t.addEventListener("mouseover", () => {
+        t.innerText = "T";
+      });
+      t.addEventListener("mouseout", () => {
+        t.innerText = "TESHENA";
+      });
+
+    case 20:
+      let u = document.getElementById("starting-with-u");
+      u.addEventListener("mouseover", () => {
+        u.innerText = "U";
+      });
+      u.addEventListener("mouseout", () => {
+        u.innerText = "UKWANI";
+      });
+
+    case 21:
+      let v = document.getElementById("starting-with-v");
+      v.addEventListener("mouseover", () => {
+        v.innerText = "V";
+      });
+      v.addEventListener("mouseout", () => {
+        v.innerText = "VEMGO";
+      });
+
+    case 22:
+      let w = document.getElementById("starting-with-w");
+      w.addEventListener("mouseover", () => {
+        w.innerText = "W";
+      });
+      w.addEventListener("mouseout", () => {
+        w.innerText = "WURKUN";
+      });
+
+    case 23:
+      let y = document.getElementById("starting-with-y");
+      y.addEventListener("mouseover", () => {
+        y.innerText = "Y";
+      });
+      y.addEventListener("mouseout", () => {
+        y.innerText = "YORUBA";
+      });
+
+    case 24:
+      let z = document.getElementById("starting-with-z");
+      z.addEventListener("mouseover", () => {
+        z.innerText = "Z";
+      });
+      z.addEventListener("mouseout", () => {
+        z.innerText = "ZARMA";
+      });
   }
 }
 
